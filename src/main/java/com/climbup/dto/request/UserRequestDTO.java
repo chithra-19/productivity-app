89ce8a2db2beb45758291140f2e8446532805ed5
Chithra <chithra201923@gmail.com>
2026-04-23 13:16:37 +0530

    debugged registerpage

--- src/main/java/com/climbup/dto/request/UserRequestDTO.java
+++ src/main/java/com/climbup/dto/request/UserRequestDTO.java
@@ -6,9 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public class UserRequestDTO {
 
-    @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
-    @NotBlank(message = "First name is required")
+   
     private String firstName;
 
 	@NotBlank(message = "Email is required")
